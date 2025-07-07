7f1246d7179d90c7056298675970638f894584fc
Renanbueno7 <renanbcamargo7@gmail.com>
2025-07-07 11:17:27 -0300

    first commit

---
+++ api_rest/views.py
@@ -0,0 +1,85 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse 
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import User
+from .serializers import UserSerializer
+
+import json 
+
+
+@api_view(['GET']) 
+def get_users(request):
+
+  if request.method == 'GET':
+
+    
+    users = User.objects.all()
+
+    serializer = UserSerializer(users, many=True)
+
+    
+    return Response(serializer.data) 
+
+  return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def databaseEmDjango():
+
+#   data = User.objects.get(pk='gabriel_nick')
+
+#   data  = User.objects.filter(user_age='25') 
+
+#   data = User.objects.exclude(user_age= '25') 
+
+  
+
+#   data.save()
+
+  #data.delete()
